ff9d487fea50537a32a5f0e07a0a57d219c8e4fd
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-02-11 17:50:34 +0200

    feat: integrate logger with server startup

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,8 @@
 import app from './app';
+import logger from '../utils/logger';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    logger.info(`Zimra FDMS Mock Server is running on port ${PORT}`);
 });
