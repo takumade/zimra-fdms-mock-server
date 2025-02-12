15c2b544c30d3209a1783f8e5c47a408d6552403
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-02-12 23:46:15 +0200

    Refactor import all device types

--- src/controllers/Device/index.ts
+++ src/controllers/Device/index.ts
@@ -2,173 +2,19 @@ import { Request, Response } from 'express';
 
 import generateOperationID from '../../utils/generateOperationID';
 
-interface OpenDayRequest {
-    // Add properties for the request body, example:
-    fiscalDayNo: number;
-    fiscalDayOpened: string;
-}
 
+import {
+  CloseDayRequest,
+  GetConfigResponse,
+  IssueCertificateRequest,
+  OpenDayRequest,
+  PingResponse,
+  SubmitFileRequest,
+  SubmitFileResponse,
+  SubmitReceiptRequest,
+  SubmittedFileHeaderDtoListResponse
+} from './device.types';
 
-interface FiscalCounter {
-    fiscalCounterType: "SaleByTax" | "SaleTaxByTax" | "CreditNoteByTax" | "CreditNoteTaxByTax" | "DebitNoteByTax" | "DebitNoteTaxByTax" | "BalanceByMoneyType" | "PayoutByTax" | "PayoutTaxByTax";
-    fiscalCounterCurrency: string;
-    fiscalCounterTaxPercent?: number;
-    fiscalCounterTaxID?: number;
-    fiscalCounterMoneyType?: "Cash" | "CreditCard" | "DebitCard" | "Check" | "WireTransfer" | "MobileMoney" | "Voucher" | "Other";
-    fiscalCounterValue: number;
-}
-  
-
-interface CloseDayRequest {
-    fiscalDayNo: number;
-    fiscalDayCounters: FiscalCounter[];
-    fiscalDayDeviceSignature: {
-        "hash": string,
-        "signature": string
-    };
-    receiptCounter: number;
-}
-
-interface IssueCertificateRequest {
-    certificateRequest: string
-}
-
-interface PingResponse {
-    reportingFrequency: number;
-    operationID: string;
-  }
-
-  interface SubmittedFileHeaderDtoListResponse {
-    files: Array<{
-      fileName: string;
-      uploadedAt: string;
-      status: string;
-    }>;
-  }
-
-interface SubmitReceiptRequest {
-        receiptType: "FiscalInvoice" | "FiscalInvoiceProforma" | "FiscalReceipt" | "NonFiscalReceipt" | "CreditNote" | "DebitNote" | "CreditNoteProforma" | "DebitNoteProforma";
-        receiptCurrency: string;
-        receiptCounter: number;
-        receiptGlobalNo: number;
-        invoiceNo: string;
-        buyerData?: {
-            buyerRegisterName: string;
-            buyerTradeName: string;
-            vatNumber?: string;
-            buyerTIN?: string;
-            buyerContacts?: {
-                phoneNo?: string;
-                email?: string;
-            };
-            buyerAddress?: {
-                province?: string;
-                city?: string;
-                street?: string;
-                houseNo?: string;
-                district?: string;
-            };
-        };
-        receiptNotes?: string;
-        receiptDate: string;
-        creditDebitNote?: {
-            receiptID?: number;
-            deviceID?: number;
-            receiptGlobalNo?: number;
-            fiscalDayNo?: number;
-        };
-        receiptLinesTaxInclusive: boolean;
-        receiptLines: Array<{
-            receiptLineType: "Sale" | "Discount";
-            receiptLineNo: number;
-            receiptLineHSCode?: string;
-            receiptLineName: string;
-            receiptLinePrice?: number;
-            receiptLineQuantity: number;
-            receiptLineTotal: number;
-            taxCode?: string;
-            taxPercent?: number;
-            taxID: number;
-        }>;
-        receiptTaxes: Array<{
-            taxCode?: string;
-            taxPercent?: number;
-            taxID: number;
-            taxAmount: number;
-            salesAmountWithTax: number;
-        }>;
-        receiptPayments: Array<{
-            moneyTypeCode: "Cash" | "CreditCard" | "DebitCard" | "Check" | "WireTransfer" | "MobileMoney" | "Voucher" | "Other";
-            paymentAmount: number;
-        }>;
-        receiptTotal: number;
-        receiptPrintForm?: "Receipt48" | "InvoiceA4";
-        receiptDeviceSignature: {
-            hash: string;
-            signature: string;
-        };
-  }
-  
-  interface Receipt {
-    receiptType: string;
-    receiptCurrency: string;
-    receiptCounter: number;
-    receiptGlobalNo: number;
-    invoiceNo: string;
-    receiptDate: string;
-    receiptLinesTaxInclusive: boolean;
-    receiptLines: Array<{
-      receiptLineType: string;
-      receiptLineNo: number;
-      receiptLineHSCode: string;
-      receiptLineName: string;
-      receiptLinePrice: number;
-      receiptLineQuantity: number;
-      receiptLineTotal: number;
-      taxCode: string;
-      taxPercent: number;
-      taxID: number;
-    }>;
-    receiptTaxes: Array<{
-      taxCode: string;
-      taxPercent: number;
-      taxID: number;
-      taxAmount: number;
-      salesAmountWithTax: number;
-    }>;
-    receiptPayments: Array<{
-      moneyTypeCode: string;
-      paymentAmount: number;
-    }>;
-    receiptTotal: number;
-    receiptPrintForm: string;
-    receiptDeviceSignature: {
-      hash: string;
-      signature: string;
-    };
-  }
-
-  interface RequestBody {
-    header: {
-      deviceId: number;
-      fiscalDayNo: number;
-      fiscalDayOpened: string;
-      fileSequence: number;
-    };
-    content: {
-      receipts: Receipt[];
-    };
-    footer: {
-      fiscalDayCounters: Array<FiscalCounter>;
-      fiscalDayDeviceSignature: {
-        hash: string;
-        signature: string;
-      };
-      receiptCounter: number;
-      fiscalDayClosed: string;
-    };
-  }
-  
 
 
 export const getConfig = async (req: Request, res: Response) => {
