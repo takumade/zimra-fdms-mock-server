33bf431a553b1cba921b7c8432060f04c7074130
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-02-14 22:45:25 +0200

    Clean code

--- src/routes/Device/index.ts
+++ src/routes/Device/index.ts
@@ -2,24 +2,19 @@ import { Router } from 'express';
 
 // @ts-ignore
 
-import { getConfig, getStatus, openDay, closeDay, issueCertificate, ping, submitReciept, submitFile, submittedFileList } from '../../controllers/Device';
-
-
+import * as deviceController from '../../controllers/Device';
 
 const router = Router();
 
-// Add your device routes here
-// Example: router.get('/', deviceController.getAllDevices);
-
-router.get('/v1/:deviceID/GetConfig', getConfig);
-router.get('/v1/:deviceID/GetStatus', getStatus);
-router.post('/v1/:deviceID/OpenDay', openDay);
-router.post('/v1/:deviceID/CloseDay', closeDay);
-router.post('/v1/:deviceID/IssueCertificate', issueCertificate);
-router.post('/v1/:deviceID/SubmitReciept', submitReciept);
-router.post('/v1/:deviceID/Ping', ping);
-router.post('/v1/:deviceID/SubmitFile', submitFile);
-router.get('/v1/:deviceID/SubmittedFileList', submittedFileList);
+router.get('/v1/:deviceID/GetConfig', deviceController.getConfig);
+router.get('/v1/:deviceID/GetStatus', deviceController.getStatus);
+router.post('/v1/:deviceID/OpenDay', deviceController.openDay);
+router.post('/v1/:deviceID/CloseDay', deviceController.closeDay);
+router.post('/v1/:deviceID/IssueCertificate', deviceController.issueCertificate);
+router.post('/v1/:deviceID/SubmitReciept', deviceController.submitReciept);
+router.post('/v1/:deviceID/Ping', deviceController.ping);
+router.post('/v1/:deviceID/SubmitFile', deviceController.submitFile);
+router.get('/v1/:deviceID/SubmittedFileList', deviceController.submittedFileList);
 
 
 export { router as default };
--- src/routes/Public/index.ts
+++ src/routes/Public/index.ts
@@ -5,11 +5,8 @@ import * as publicController from '../../controllers/Public';
 
 const router = Router();
 
-// Add your public routes here
-// Example: router.get('/', publicController.getPublicInfo);
-
 router.post('/v1/:deviceID/RegisterDevice', publicController.registerDevice);
-router.get('v1/GetServerCertificate', publicController.getServerCertificate);
+router.get('/v1/GetServerCertificate', publicController.getServerCertificate);
 router.post('/v1/:deviceID/VerifyTaxpayerInformation', publicController.verifyTaxpayerInformation);
 
 export { router as default };
