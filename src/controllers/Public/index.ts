eafccb63995509f9f5730e8b5b4b4f48f1ffeffc
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-02-14 22:26:36 +0200

    Add register device code

--- src/controllers/Public/index.ts
+++ src/controllers/Public/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import generateOperationID from '../../utils/generateOperationID';
 
 // Add your public controller methods here
 // Example:
@@ -10,4 +11,61 @@ import { Request, Response } from 'express';
 //     }
 // };
 
+export const registerDevice  = async (req: Request, res: Response) => {
+    const { deviceID } = req.params;
+    const { DeviceModelName, DeviceModelVersion } = req.headers;
+    const requestBody = req.body;
+  
+    if (!deviceID || isNaN(Number(deviceID))) {
+      return res.status(400).json({
+        errorCode: "BAD_REQUEST",
+        type: "https://httpstatuses.io/400",
+        title: "Bad Request",
+        status: 400,
+        detail: "Invalid or missing device ID",
+        operationID: generateOperationID(),
+      });
+    }
+  
+    if (!DeviceModelName || !DeviceModelVersion) {
+      return res.status(400).json({
+        errorCode: "BAD_REQUEST",
+        type: "https://httpstatuses.io/400",
+        title: "Bad Request",
+        status: 400,
+        detail: "Missing required headers",
+        operationID: generateOperationID(),
+      });
+    }
+  
+    if (!requestBody) {
+      return res.status(422).json({
+        errorCode: "DEV01",
+        type: "https://httpstatuses.io/422",
+        title: "Unprocessable Entity",
+        status: 422,
+        detail: "Device not found or not active",
+        operationID: generateOperationID(),
+      });
+    }
+  
+    try {
+      const response = {
+        operationID: generateOperationID(),
+        certificate: "-----BEGIN CERTIFICATE-----\nMIIC6TCCAdGg...\n-----END CERTIFICATE-----",
+      };
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json({
+        type: "https://httpstatuses.io/500",
+        title: "Server error",
+        status: 500,
+        operationID: generateOperationID(),
+      });
+    }
+}
+
+export const getServerCertificate  = async (req: Request, res: Response) => {}
+
+export const verifyTaxpayerInformation  = async (req: Request, res: Response) => {}
 
