d5b1cc4a7fd208be05c1ceb85f5346d45aa96ca2
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-02-17 21:27:28 +0200

    Add  ttry catch

--- src/utils/checkDeviceID.ts
+++ src/utils/checkDeviceID.ts
@@ -1,9 +1,14 @@
 const checkDeviceID = (id: number | string): boolean => {
-    const idNumber = Number(id);
-    if (isNaN(idNumber) || idNumber <= 0 || idNumber >= 10000000000 || String(idNumber).length !== String(id).length) {
+
+    try {
+        const idNumber = Number(id);
+        if (isNaN(idNumber) || idNumber <= 0 || idNumber >= 10000000000 || String(idNumber).length !== String(id).length) {
+            return false;
+        }
+        return true;
+    } catch (error) {
         return false;
     }
-    return true;
 };
 
 
