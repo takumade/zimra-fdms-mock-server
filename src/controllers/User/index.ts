0ec6228803b2d7bd171483b47c940f86b9fdc6ee
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-02-17 21:45:16 +0200

    Get users list endpoint

--- src/controllers/User/index.ts
+++ src/controllers/User/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import generateOperationID from '../../utils/generateOperationID';
 
 // Add your user controller methods here
 // Example:
@@ -9,3 +10,32 @@ import { Request, Response } from 'express';
 //         res.status(500).json({ error: 'Internal server error' });
 //     }
 // };
+
+
+export const getUsersList = (req: Request, res: Response) => {
+    const { deviceID } = req.params;
+    const { Sort, Order, Offset, Limit, Operator } = req.query;
+    const DeviceModelName = req.header("DeviceModelName");
+    const DeviceModelVersion = req.header("DeviceModelVersion");
+  
+    if (!deviceID || !Offset || !Limit) {
+      return res.status(400).json({ message: "Missing required parameters" });
+    }
+  
+    res.status(200).json({
+      operationID: generateOperationID(),
+      total: 10,
+      rows: [
+        {
+          userName: "mockUser",
+          personName: "John",
+          personSurname: "Doe",
+          userRole: "Admin",
+          email: "john.doe@example.com",
+          phoneNo: "+123456789",
+          userStatus: "Active",
+        },
+      ],
+    });
+  };
+  
