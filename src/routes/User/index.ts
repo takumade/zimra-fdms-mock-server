bbddea1e75b49afebf313e636c5b1422a41a5e72
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-02-17 21:34:16 +0200

    Add more endpoints

--- src/routes/User/index.ts
+++ src/routes/User/index.ts
@@ -13,4 +13,12 @@ router.post('/v1/:deviceID/CreateUser', () =>{});
 router.post('/v1/:deviceID/Login', () =>{});
 router.post('/v1/:deviceID/SendSecurityCodeToUserPhone', () =>{});
 router.post('/v1/:deviceID/SendSecurityCodeToUserEmail', () =>{});
+router.post('/v1/:deviceID/ConfirmUser', () =>{});
+router.post('/v1/:deviceID/ChangePassword', () =>{});
+router.post('/v1/:deviceID/ResetPassword', () =>{});
+router.post('/v1/:deviceID/ConfirmContact', () =>{});
+router.post('/v1/:deviceID/Update', () =>{});
+router.post('/v1/:deviceID/ConfirmPasswordReset', () =>{});
+
+
 export { router as default };
