94241181e9bc1e4047723dd39e5a9cfe6fa913af
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-02-17 21:30:11 +0200

    Add get users list endpoind

--- src/routes/User/index.ts
+++ src/routes/User/index.ts
@@ -6,4 +6,7 @@ const router = Router();
 // Add your user routes here
 // Example: router.get('/', userController.getAllUsers);
 
+
+router.get('/v1/:deviceID/GetUsersList', () =>{});
+
 export { router as default };
