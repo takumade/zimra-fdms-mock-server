e8302d9c0926227dc126a4c88438688760cac836
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-02-17 21:26:19 +0200

    Correct return type

--- src/utils/checkDeviceID.ts
+++ src/utils/checkDeviceID.ts
@@ -1,4 +1,4 @@
-const checkDeviceID = (id: number | string): string => {
+const checkDeviceID = (id: number | string): boolean => {
     const idNumber = Number(id);
     if (isNaN(idNumber) || idNumber <= 0 || idNumber >= 10000000000 || String(idNumber).length !== String(id).length) {
         return false;
